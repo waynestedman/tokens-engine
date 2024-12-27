747d93340c731a3ae4ad84ec9b3b23b1ba827533
Wayne Stedman <waynestedman@users.noreply.github.com>
2024-12-26 16:43:33 -0800

    basic tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -4,93 +4,106 @@
 
 export const ColorsBlack = "#000000";
 export const ColorsWhite = "#ffffff";
-export const ColorsOrange100 = "#fffaf0";
-export const ColorsOrange200 = "#feebc8";
-export const ColorsOrange300 = "#fbd38d";
-export const ColorsOrange400 = "#f6ad55";
-export const ColorsOrange500 = "#ed8936";
-export const ColorsOrange600 = "#dd6b20";
-export const ColorsOrange700 = "#c05621";
-export const ColorsOrange800 = "#9c4221";
-export const ColorsOrange900 = "#7b341e";
+export const ColorsGray100 = "#e8e8ea";
+export const ColorsGray200 = "#c5c2c3";
+export const ColorsGray300 = "#807b7e";
+export const ColorsGray400 = "#615b5f";
+export const ColorsGray500 = "#554f53";
+export const ColorsSage100 = "#e5e6de";
+export const ColorsSage200 = "#838577";
+export const ColorsSage300 = "#626456";
 export const Dimensions0 = "0px";
-export const Dimensions1 = "4px";
-export const Dimensions2 = "8px";
-export const Dimensions3 = "12px";
-export const Dimensions4 = "16px";
-export const Dimensions5 = "20px";
-export const Dimensions6 = "24px";
-export const Dimensions7 = "28px";
-export const Dimensions8 = "32px";
-export const Dimensions9 = "36px";
-export const Dimensions10 = "40px";
-export const Dimensions11 = "44px";
-export const Dimensions12 = "48px";
-export const Dimensions13 = "52px";
-export const Dimensions14 = "56px";
-export const Dimensions15 = "60px";
+export const Dimensions4 = "4px";
+export const Dimensions8 = "8px";
+export const Dimensions10 = "10px";
+export const Dimensions12 = "12px";
+export const Dimensions14 = "14px";
+export const Dimensions16 = "16px";
+export const Dimensions20 = "20px";
+export const Dimensions22 = "22px";
+export const Dimensions24 = "24px";
+export const Dimensions28 = "28px";
+export const Dimensions30 = "30px";
+export const Dimensions32 = "32px";
+export const Dimensions36 = "36px";
+export const Dimensions40 = "40px";
+export const Dimensions44 = "44px";
+export const Dimensions48 = "48px";
+export const Dimensions52 = "52px";
+export const Dimensions54 = "54px";
+export const Dimensions56 = "56px";
+export const Dimensions60 = "60px";
+export const Dimensions72 = "72px";
 export const DimensionsMax = "9999px";
-export const TextTypographyHeading = {
-  fontFamily: "Open Sans, sans-serif",
-  fontWeight: "extra-bold",
-  fontSize: "28px",
-  lineHeight: 1.8,
-};
-export const TextTypographyBody = {
-  fontFamily: "Times New Roman, serif",
-  fontWeight: "regular",
-  fontSize: "16px",
-  lineHeight: 1.2,
-};
-export const TextFontsSerif = "Times New Roman, serif";
-export const TextFontsSans = "Open Sans, sans-serif";
-export const TextSizesHeading = "28px";
-export const TextSizesBody = "16px";
-export const TextWeightsLight = "thin";
-export const TextWeightsRegular = "regular";
-export const TextWeightsBold = "extra-bold";
-export const TextLineHeightsNormal = 1.2;
+export const TextFontsBitter = "Bitter";
+export const TextFontsRaleway = "Raleway";
+export const TextSizesHeading1 = "72px";
+export const TextSizesHeading2 = "54px";
+export const TextSizesHeading3 = "40px";
+export const TextSizesHeading4 = "30px";
+export const TextSizesHeading5 = "22px";
+export const TextSizesHeading6 = "16px";
+export const TextSizesParagraph = "16px";
+export const TextSizesCaption = "14px";
+export const TextSizesEyebrow = "14px";
+export const TextWeights300 = "300";
+export const TextWeights400 = "400";
+export const TextWeights500 = "500";
+export const TextWeights700 = "700";
+export const TextLineHeightsNormal = 1.5;
+export const TextLineHeightsSmall = 1.25;
 export const TextLineHeightsLarge = 1.8;
-export const TransitionsEmphasis = {
-  duration: "300ms",
-  delay: "0ms",
-  timingFunction: [0.5, 0, 1, 1],
+export const TypographyHeading1 = {
+  fontFamily: "Raleway",
+  fontWeight: "700",
+  fontSize: "72px",
+  lineHeight: 1.5,
 };
-export const TransitionsFade = {
-  duration: "600ms",
-  delay: "0ms",
-  timingFunction: [0, 0, 0.5, 1],
+export const TypographyHeading2 = {
+  fontFamily: "Raleway",
+  fontWeight: "700",
+  fontSize: "54px",
+  lineHeight: 1.5,
 };
-export const TransitionsEasingFunctionsAccelerate = [0.5, 0, 1, 1];
-export const TransitionsEasingFunctionsDecelerate = [0, 0, 0.5, 1];
-export const TransitionsDurationsInstant = "0ms";
-export const TransitionsDurationsShort = "100ms";
-export const TransitionsDurationsMedium = "300ms";
-export const TransitionsDurationsLong = "600ms";
-export const BordersHeavy = { color: "#000000", width: "4px", style: "solid" };
-export const BordersWireframe = {
-  color: "#dd6b20",
-  width: "8px",
-  style: { dashArray: ["0.5rem", "0.25rem"], lineCap: "round" },
+export const TypographyHeading3 = {
+  fontFamily: "Raleway",
+  fontWeight: "500",
+  fontSize: "40px",
+  lineHeight: 1.25,
 };
-export const BordersStylesSolid = "solid";
-export const BordersStylesDashed = {
-  dashArray: ["0.5rem", "0.25rem"],
-  lineCap: "round",
+export const TypographyHeading4 = {
+  fontFamily: "Raleway",
+  fontWeight: "500",
+  fontSize: "30px",
+  lineHeight: 1.25,
 };
-export const ShadowsSm = {
-  color: "#000000",
-  offsetX: "0px",
-  offsetY: "4px",
-  blur: "12px",
+export const TypographyHeading5 = {
+  fontFamily: "Raleway",
+  fontWeight: "500",
+  fontSize: "22px",
+  lineHeight: 1.5,
 };
-export const ShadowsLg = {
-  color: "#000000",
-  offsetX: "0px",
-  offsetY: "8px",
-  blur: "16px",
+export const TypographyHeading6 = {
+  fontFamily: "Raleway",
+  fontWeight: "500",
+  fontSize: "16px",
+  lineHeight: 1.5,
 };
-export const ShadowsMulti = [
-  { color: "#000000", offsetX: "0px", offsetY: "4px", blur: "12px" },
-  { color: "#000000", offsetX: "0px", offsetY: "8px", blur: "16px" },
-];
+export const TypographyBody = {
+  fontFamily: "Bitter",
+  fontWeight: "400",
+  fontSize: "16px",
+  lineHeight: 1.5,
+};
+export const TypographyCaption = {
+  fontFamily: "Bitter",
+  fontWeight: "300",
+  fontSize: "14px",
+  lineHeight: 1.5,
+};
+export const TypographyEyebrow = {
+  fontFamily: "Raleway",
+  fontWeight: "400",
+  fontSize: "14px",
+  lineHeight: 1.5,
+};
